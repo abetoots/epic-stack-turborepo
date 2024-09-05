001ba85c1c2659a7ab4d46496919a7a07a3b494f
Abe Caymo <caymo.abesuni@gmail.com>
2024-09-05 15:19:44 +0800

    fix: tailwind theme

--- apps/web/app/utils/extended-theme.ts
+++ apps/web/app/utils/extended-theme.ts
@@ -89,14 +89,14 @@ export const extendedTheme = {
 		caption: ['1.125rem', { lineHeight: '1.5rem', fontWeight: '600' }],
 		/** 12px size / 16px high / bold */
 		button: ['0.75rem', { lineHeight: '1rem', fontWeight: '700' }],
-		keyframes: {
-			'caret-blink': {
-				'0%,70%,100%': { opacity: '1' },
-				'20%,50%': { opacity: '0' },
-			},
+	},
+	keyframes: {
+		'caret-blink': {
+			'0%,70%,100%': { opacity: '1' },
+			'20%,50%': { opacity: '0' },
 		},
-		animation: {
-			'caret-blink': 'caret-blink 1.25s ease-out infinite',
-		},
+	},
+	animation: {
+		'caret-blink': 'caret-blink 1.25s ease-out infinite',
 	},
 } satisfies Config['theme']
